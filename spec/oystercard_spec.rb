1838143487f613b9dbec0ae0514edd5d6acbec4a
Monaliza <mo.o@hotmail.co.uk>
2021-10-25 16:26:36 +0100

    step 4 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,18 @@
+require_relative './../lib/oystercard.rb'
 describe Oystercard do
-
+    it 'balance default value 0' do
+        expect(subject.balance).to eq (0)
+    end
 end
+
+
+
+
+
+
+
+
+
+# failure error -unintialised constant oystercard 
+# file path: # ./spec/oystercard_spec.rb:1:in `<top (required)>'
+# create lib folder, with class oystercard and that should fix the error. 
